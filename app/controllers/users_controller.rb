f4b03502c870cc9c96acc954d9385e61921f1db2
Kenchiki-chiki <kodama303@gmail.com>
2020-06-29 17:14:44 +0900

    編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+  end
+
+  
+
 
   private
   def user_params
