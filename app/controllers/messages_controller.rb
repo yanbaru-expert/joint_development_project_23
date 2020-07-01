448f01e921d3813d19931c586ce02a40f66798dc
Yuta Suzuki <yutasuzuki@YMBP.local>
2020-07-01 22:29:41 +0900

    新規投稿機能を実装しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  def new
+    @message = Message.new
+  end
+  
+  def create
+    Message.create(user_params)
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :content)
+  end
 end
