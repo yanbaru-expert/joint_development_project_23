e1e0a4b09d9a9b7df9c6c5bb2ae5893d50c80b03
Yuta Suzuki <yutasuzuki@YMBP.local>
2020-07-02 10:12:38 +0900

    indexを実装しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,18 @@
 class MessagesController < ApplicationController
+  
+  def index
+    @messages = Message.all
+  end
+
   def new
     @message = Message.new
   end
   
   def create
-    Message.create(user_params)
+    Message.create(message_params)
   end
-
+  
+  private
   def message_params
     params.require(:message).permit(:title, :content)
   end
