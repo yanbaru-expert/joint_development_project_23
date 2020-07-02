edb509ea5364dcbb13136745e041c5cf4accb89b
Yuta Suzuki <yutasuzuki@YMBP.local>
2020-07-02 14:50:01 +0900

    destroy機能実装し直しました

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,20 +11,26 @@ class MessagesController < ApplicationController
   def create
     Message.create(message_params)
   end
-
-  def edit
-    @message = Message.find(params[:id])
-  end
-
-  def update
-    message = Message.find(params[:id])
-    message.update(message_params)
-  end
-
+  
   def show
     @message = Message.find(params[:id])
   end
 
+  
+  def edit
+    @message = Message.find(params[:id])
+  end
+  
+  def update
+    message = Message.find(params[:id])
+    message.update(message_params)
+  end
+  
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+  end
+  
   private
   def message_params
     params.require(:message).permit(:title, :content)
