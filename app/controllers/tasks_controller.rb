2235ce05ba26a4133d3b6dd39c6409ec7aa453e2
mai Matsuda <mai117@mai-no-MacBook-Pro.local>
2020-07-04 18:55:42 +0900

    新規投稿絹を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end
