817637abd950efaa31c7e2356e73c3003badf075
mai Matsuda <mai117@mai-no-MacBook-Pro.local>
2020-07-04 19:55:13 +0900

    詳細表示機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,18 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
   end
 
   def create
     Task.create(task_params)
+  end
+  
+  def show
+    @task = Task.find(params[:id])
   end
 
   private
